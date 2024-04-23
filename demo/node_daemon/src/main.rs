5fe1a87519c32a94ab87de29c3c69eb2d4c196d5
Liu Zhicong <waterflier@gmail.com>
2024-04-22 23:33:36 -0700

    pass compile

--- demo/node_daemon/src/main.rs
+++ demo/node_daemon/src/main.rs
@@ -1,6 +1,31 @@
 use log::*;
 use simplelog::*;
 use std::fs::File;
+use tokio::*;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum NodeDaemonErrors {
+    #[error("Failed due to reason: {0}")]
+    ReasonError(String),
+    #[error("File not found: {0}")]
+    FileNotFoundError(String),
+    // 其他错误类型
+}
+
+type Result<T> = std::result::Result<T, NodeDaemonErrors>;
+
+struct NodeIdentityConfig {
+
+}
+
+struct ZoneConfig {
+    etcd_version : u64,
+}
+
+struct EtcdState {
+    run_in_this_machine : bool,
+}
 
 fn init_log_config() {
     // 创建一个日志配置对象
@@ -15,74 +40,114 @@ fn init_log_config() {
     ]).unwrap();
 }
 
-fn load_identity_config() -> Result<(), Box<dyn std::error::Error>>{
-
+fn load_identity_config() -> Result<NodeIdentityConfig> {
+    unimplemented!();
 } 
 
-fn looking_zone_config() -> Result<(), Box<dyn std::error::Error>>{
-
+async fn looking_zone_config() -> Result<ZoneConfig> {
+    unimplemented!();
 }
 
 
-async fn check_etcd_by_zone_config(config) -> Result<(), Box<dyn std::error::Error>>{
-
+async fn check_etcd_by_zone_config(config:&ZoneConfig) -> Result<EtcdState> {
+    unimplemented!();
 }
 
-fn execute_docker(docker_config)   -> Result<(), Box<dyn std::error::Error>>{
-    for docker_instance in docker_config {
+
+async fn check_etcd_data()->Result<bool> {
+    unimplemented!();
+}
+
+async fn get_etcd_data_version()->Result<u64> {
+    unimplemented!();
+}
+
+async fn try_start_etcd()->Result<()> {
+    unimplemented!();
+}
+
+async fn try_restore_etcd()->Result<()> {
+    unimplemented!();
+}
+
+//fn execute_docker(docker_config)   -> Result<(), Box<dyn std::error::Error>>{
+//    for docker_instance in docker_config {
         //尝试启动/停止镜像
         //启动镜像前，需要通知zone内的docker repo先更新必要的镜像。该过程和docekr repo的实现是解耦合的，后续可以用
-    }
-}
+//    }
+//}
 
-fn execute_service(service_config)  -> Result<(), Box<dyn std::error::Error>>{
-    for service_instance in service_config {
+//fn execute_service(service_config)  -> Result<(), Box<dyn std::error::Error>>{
+//    for service_instance in service_config {
         //service一定不跑在docker里
         //尝试启动/停止/更新服务
         
-    }
+//    }
+//}
+
+
+async fn node_daemon_main_loop(config:ZoneConfig) -> Result<()> {
+    //etcd_client = create_etcd_client()
+    //etcd_client.refresh_config()
+    //system_config.init()
+    //cmd_config = system_config.get("")
+    //execute_cmd(cmd_config) //一般是执行运维命令，类似系统备份和恢复
+    //service_config = system_config.get("")
+    //execute_service(service_config)
+    //vm_config = system_config.get("")
+    //execute_vm(vm_config)
+    //docker_config = system_config.get("")
+    //execute_docker(docker_config)
+    unimplemented!();
 }
 
 
-fn node_daemon_main_loop() -> Result<(), Box<dyn std::error::Error>>{
-    etcd_client = create_etcd_client()
-    etcd_client.refresh_config()
-    system_config.init()
-    cmd_config = system_config.get("")
-    execute_cmd(cmd_config) //一般是执行运维命令，类似系统备份和恢复
-    service_config = system_config.get("")
-    execute_service(service_config)
-    vm_config = system_config.get("")
-    execute_vm(vm_config)
-    docker_config = system_config.get("")
-    execute_docker(docker_config)
-}
-
-
-fn main() {
+#[tokio::main]
+async fn main() -> std::result::Result<(), String> {
     init_log_config();
     info!("node_dameon start!");
 
-    if load_identity_config().is_err() {
-        error!("load identity config failed!");
-        return;
-    }
+    let node_identity = load_identity_config().map_err(|err|{
+        error!("load node identity config failed!");
+        String::from("load node identity config failed!")
+    })?;
 
-    zone_config = looking_zone_config();
+    let zone_config = looking_zone_config().await.map_err(|err|{
+        error!("looking zone config failed!");
+        String::from("looking zone config failed!")
+    })?;
     
-    //检查
-    etcd_state = check_etcd_by_zone_config(zone_config).await;
+    //检查etcd状态
+    let etcd_state = check_etcd_by_zone_config(&zone_config).await.map_err(|err|{
+        error!("check etcd by zone config failed!");
+        return String::from("check etcd by zone config failed!");
+    })?;
 
     //判断etcd的版本是否大于zone_config里的etcd版本，如果大于说明etcd的数据是新的，否则就要等待 etcd完成更新
     if etcd_state.run_in_this_machine {
-        if check_etcd_data() == need_restore{
-            restore_etcd_data();//这里是为Zone恢复数据，是独立流程。Node恢复自己的数据
+        let etcd_data_version = get_etcd_data_version().await.map_err(|err|{
+            error!("get etcd data version failed!");
+            return String::from("get etcd data version failed!");
+        })?;
+
+        if etcd_data_version < zone_config.etcd_version {
+            info!("local etcd data version is old, wait for etcd restore!");
+            try_restore_etcd().await.map_err(|err|{
+                error!("try restore etcd failed!");
+                return String::from("try restore etcd failed!");
+            })?;
         }
 
-        start_etcd()
+        try_start_etcd().await.map_err(|err| {
+            error!("try start etcd failed!");
+            return String::from("try start etcd failed!");
+        })?;
     }
 
-    //从系统的高一致性的要重复啊
-    node_daemon_main_loop()
-    
+    node_daemon_main_loop(zone_config).await.map_err(|err|{
+        error!("node daemon main loop failed!");
+        return String::from("node daemon main loop failed!");
+    })?;
+
+    Ok(())
 }
