9561b247fd89b2098cd88db557f2be3788d81c7f
sunxinle <alexsunxl@163.com>
2024-04-24 16:44:20 +0800

    feature: etcd client snapshot

--- demo/etcd_client/src/lib.rs
+++ demo/etcd_client/src/lib.rs
@@ -1,6 +1,8 @@
 use std::vec;
 
-use etcd_rs::{Client, ClientConfig, Endpoint};
+use etcd_rs::{Client, ClientConfig, Endpoint, KeyValueOp};
+use serde_json::json;
+use tokio::fs::write;
 
 pub struct EtcdClient {
     pub client: Client,
@@ -14,5 +16,26 @@ impl EtcdClient {
             Ok(client) => Ok(EtcdClient { client }),
             Err(e) => Err(Box::new(e)),
         }
+    }
+
+    // backup snapshot to local file
+    pub async fn snapshot(&self) -> Result<(), Box<dyn std::error::Error>> {
+        // let options = GetOptions::new().with_range("0", "z");
+        let response = self.client.get_all().await?;
+        // 将响应中的键值对转换为 JSON
+        let kv_pairs = response
+            .kvs
+            .iter()
+            .map(|kv| {
+                json!({
+                    "key": String::from(kv.key_str()),
+                    "value": String::from(kv.value_str())
+                })
+            })
+            .collect::<Vec<_>>();
+        let serialized_data = serde_json::to_string_pretty(&kv_pairs)?;
+        write("etcd_snapshot.json", serialized_data).await?;
+
+        Ok(())
     }
 }
