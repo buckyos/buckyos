73909bdcf87e76190be89a98e77191ad53a2a076
Song <glen0125@foxmail.com>
2024-04-29 11:53:20 +0800

    add test code

--- demo/node_daemon/src/pkg_mgr.rs
+++ demo/node_daemon/src/pkg_mgr.rs
@@ -1,5 +1,8 @@
+use log::*;
 use regex::Regex;
 use std::path::PathBuf;
+use tide::log::info;
+use version_compare::{compare, compare_to, Cmp, Version};
 
 /*
 pkg_id由两部分组成，包名和版本号或者sha256值。例如：
@@ -10,7 +13,7 @@ pkg_name#sha256:1234567890
 pkg_name#<0.1.6, pkg_name#<=0.1.6
 pkg_name#>0.1.4<0.1.6, pkg_name#>0.1.4<=0.1.6, pkg_name#>=0.1.4<0.1.6, pkg_name#>=0.1.4<=0.1.6
  */
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct PackageId {
     pub name: String,
     pub version: Option<String>,
@@ -27,10 +30,10 @@ pkg.lock: 记录当前环境下已经安装的包精确信息
 在包安装时，需要解析pkg_id，根据pkg_id从 index_db 中获取sha256值，然后下载
 安装时，env第一级目录都是直接依赖的包，用npm举例来说，就是package.json中的dependencies，包内部依赖的包以及devDeps不会出现在这里，避免幽灵依赖
 env第一级目录中是包的软连接 和 .pkgs 目录，.pkgs目录中才是包的真实内容
-所有包的真实内容都在.pkgs的第一级目录中，第一级目录都是类似于 pkg_name#version 这样的文件夹，比如 core#v1.0.3，也就是可以同时依赖同名但版本不同的包
+所有包的真实内容都在.pkgs的第一级目录中，第一级目录都是类似于 pkg_name#version 这样的文件夹，比如 core#1.0.3，也就是可以同时依赖同名但版本不同的包
 在安装时，会先检查是否已经安装了这个包，如果已经安装了，就不再重复安装，如果没有安装，就下载，解压到.pkgs的第一级目录，然后创建软连接
 对于包内部的依赖，也都会提升到 .pkgs 的第一级目录中去查找，避免依赖地狱
-所以理论上，.pkgs 下面的文件，最多只有1级子目录，第一级是实际的包目录，第二级是包内部依赖的软连接，而软链接又会链接到第一级目录中
+所以理论上，.pkgs 下面的文件，最多只有2级子目录，第一级是实际的包目录，第二级是包内部依赖的软连接，而软链接又会链接到第一级目录中
 这里要考虑的问题就是删除一个包时需要做的操作，理论上 .pkgs 下面的第一级目录只要没有任何软链接指向它，就可以删除，定时删除或者按需删除也可以是一个方案
 还有在安装时如果安装失败是否要回退的问题。
  */
@@ -44,11 +47,13 @@ pub struct PackageEnv {
   类型（dir or file）
   完整路径
 */
+#[derive(Debug)]
 pub enum MediaType {
     Dir,
     File,
 }
 
+#[derive(Debug)]
 pub struct MediaInfo {
     pub pkg_id: PackageId,
     pub full_path: PathBuf,
@@ -80,14 +85,15 @@ impl PackageEnv {
     }
 
     // load 一个包，从env根目录中查找目标pkg，找到了就返回一个MediaInfo结构，env文件结构见末尾
-    pub async fn load_pkg(&self, pkg_id: &str) -> Result<MediaInfo> {
-        let pkg_id = self.parse_pkg_id(pkg_id)?;
+    pub async fn load_pkg(&self, pkg_id_str: &str) -> Result<MediaInfo> {
+        let pkg_id = self.parse_pkg_id(pkg_id_str)?;
         //判断work_dir下是否存在pkg_id对应的目录
         let mut full_path = self.work_dir.join(&pkg_id.name);
         if let Some(version) = &pkg_id.version {
             //如果version不是以>=,<=,>,<开头，就是精确版本号
             if !version.starts_with(">") && !version.starts_with("<") {
                 full_path.push(format!("#{}", version));
+                info!("get full path for {} -> {:?}", pkg_id_str, full_path);
 
                 return self.load_with_full_path(&pkg_id, &full_path);
             }
@@ -96,6 +102,7 @@ impl PackageEnv {
         //如果有精确的sha256值，也可以拼接
         if let Some(sha256) = &pkg_id.sha256 {
             full_path.push(format!("#{}", sha256));
+            info!("get full path for {} -> {:?}", pkg_id_str, full_path);
 
             if let Ok(media_info) = self.load_with_full_path(&pkg_id, &full_path) {
                 return Ok(media_info);
@@ -107,8 +114,8 @@ impl PackageEnv {
 
     // install 一个包，安装时一定要有确定的版本号或者sha256值
     #[async_recursion::async_recursion]
-    pub async fn install_pkg(&self, pkg_id: &str) -> Result<()> {
-        let mut pkg_id = self.parse_pkg_id(pkg_id)?;
+    pub async fn install_pkg(&self, pkg_id_str: &str) -> Result<()> {
+        let mut pkg_id = self.parse_pkg_id(pkg_id_str)?;
         if self.load_pkg(&pkg_id.name).await.is_ok() {
             return Ok(());
         }
@@ -199,7 +206,7 @@ impl PackageEnv {
 
     pub fn get_version_from_sha256(&self, pkg_name: &str, sha256: &str) -> Result<Option<String>> {
         // TODO 查询index_db
-        unimplemented!();
+        Ok(None)
     }
 
     pub fn get_sha256_from_version(&self, pkg_name: &str, version: &str) -> Result<Option<String>> {
@@ -210,7 +217,7 @@ impl PackageEnv {
     pub fn get_default_version(&self, pkg_name: &str) -> Result<Option<String>> {
         // TODO 查询package.lock中存在的版本
         // 或者查询index_db，默认获取index_db中最新的？
-        unimplemented!();
+        Ok(None)
     }
 
     fn load_with_full_path(&self, pkg_id: &PackageId, full_path: &PathBuf) -> Result<MediaInfo> {
@@ -272,9 +279,12 @@ impl PackageEnv {
             }
         }
 
+        info!("load_with_version_expression: min_version:{:?}, inclusive_min:{}, max_version:{:?}, inclusive_max:{}", 
+        min_version, inclusive_min, max_version, inclusive_max);
+
         // 找到符合条件的版本
         let matching_version = self.find_matching_version(
-            &pkg_id.name,
+            &pkg_id,
             min_version,
             max_version,
             inclusive_min,
@@ -290,13 +300,116 @@ impl PackageEnv {
 
     fn find_matching_version(
         &self,
-        pkg_name: &str,
+        pkg_id: &PackageId,
         min_version: Option<String>,
         max_version: Option<String>,
         inclusive_min: bool,
         inclusive_max: bool,
     ) -> Result<String> {
-        //这里可以查询pkg.lock或者直接遍历目录？
-        unimplemented!();
+        let pkg_name = &pkg_id.name;
+        let version_expression = &pkg_id.version;
+        let mut pkg_full_name = String::from(pkg_name);
+        if version_expression.is_some() {
+            pkg_full_name += "#";
+            pkg_full_name += version_expression.as_ref().unwrap();
+        }
+        //TODO 先查询index_db
+        let index_db_path = self.work_dir.join("index_db");
+        if index_db_path.exists() {
+            //TODO 从index_db中查询
+            todo!("query index_db");
+        }
+
+        //TODO 查询lock文件
+
+        //遍历目录，找到所有包名匹配的目录
+        let mut matching_versions: Vec<String> = Vec::new();
+        let pkgs_dir = self.work_dir.join(".pkgs");
+        if pkgs_dir.exists() {
+            for entry in pkgs_dir.read_dir().unwrap() {
+                if let Ok(entry) = entry {
+                    if entry.path().is_dir() {
+                        let file_name = entry.file_name().into_string().unwrap();
+                        if !file_name.starts_with(pkg_name) {
+                            continue;
+                        }
+                        //以#分割，取第最后一部分作为版本号
+                        let parts: Vec<&str> = file_name.split("#").collect();
+                        let version = parts[parts.len() - 1];
+                        if let Some(min_version) = &min_version {
+                            let compare_ret: Cmp =
+                                compare(&version, min_version).map_err(|_err| {
+                                    PackageSystemErrors::LoadError(
+                                        pkg_full_name.clone(),
+                                        "Compare version error".to_string(),
+                                    )
+                                })?;
+                            if compare_ret == Cmp::Lt {
+                                continue;
+                            }
+                            if compare_ret == Cmp::Eq && !inclusive_min {
+                                continue;
+                            }
+                        }
+
+                        if let Some(max_version) = &max_version {
+                            let compare_ret: Cmp =
+                                compare(&version, max_version).map_err(|_err| {
+                                    PackageSystemErrors::LoadError(
+                                        pkg_full_name.clone(),
+                                        "Compare version error".to_string(),
+                                    )
+                                })?;
+                            if compare_ret == Cmp::Gt {
+                                continue;
+                            }
+                            if compare_ret == Cmp::Eq && !inclusive_max {
+                                continue;
+                            }
+                        }
+
+                        matching_versions.push(version.to_owned());
+                    }
+                }
+            }
+            //在matching_versions中选择版本最高的
+            if matching_versions.is_empty() {
+                return Err(PackageSystemErrors::LoadError(
+                    pkg_name.to_string(),
+                    "No matching version found".to_string(),
+                ));
+            } else {
+                let mut result_version = matching_versions[0].clone();
+                for version in matching_versions {
+                    if compare(&version, &result_version).unwrap() == Cmp::Gt {
+                        result_version = version;
+                    }
+                }
+                return Ok(result_version.to_owned());
+            }
+        } else {
+            return Err(PackageSystemErrors::LoadError(
+                pkg_full_name,
+                "Not found".to_string(),
+            ));
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_load() {
+        // 获取当前文件绝对路径
+        let env_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let env: PackageEnv = PackageEnv::new(PathBuf::from(env_path));
+        let pkg_id_str = "src";
+        let pkg_id = env.parse_pkg_id(pkg_id_str).unwrap();
+        println!("parse pkg_id {} -> {:?}", pkg_id_str, pkg_id);
+        let media_info = env.load_pkg(pkg_id_str).await;
+        println!("load pkg_id {} -> {:?}", pkg_id_str, media_info);
+        assert!(media_info.is_ok());
     }
 }
