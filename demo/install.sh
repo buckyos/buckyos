a4fd47e8d322404faa1321865e4b1c51d48a350a
wugren <wuguoren@buckyos.com>
2024-05-28 17:25:13 +0800

    support local zone config

--- demo/install.sh
+++ demo/install.sh
@@ -53,41 +53,95 @@ main() {
 # 	docker_image="buckyos:latest"
 
 	read -p "Please enter your zone name: " zone_name < /dev/tty
+	zone_cfg_mode="dns"
+	zone_cfg_check=$($buckycli check_dns $zone_name)
+	case $zone_cfg_check in
+		"valid" )
+			echo "The zone config is valid."
+			zone_cfg=$($buckycli query_dns $zone_name)
+			node_1=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[0].name')
+			node_2=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[1].name')
+			node_3=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[2].name')
+			echo "The name of node 1 is $node_1."
+			echo "The name of node 2 is $node_2."
+			echo "The name of node 3 is $node_3."
+			;;
+		"invalid")
+			echo "Zone configuration on DNS is not available."
+			read -p "Please enter the name of node 1 [default: etcd1]: " node_1 < /dev/tty
+			read -p "Please enter the name of node 2 [default: etcd2]: " node_2 < /dev/tty
+			read -p "Please enter the name of node 3 [default: gateway]: " node_3 < /dev/tty
+			if [ -z "$node_1" ]; then
+				node_1="etcd1"
+			fi
+			if [ -z "$node_2" ]; then
+				node_2="etcd2"
+			fi
+			if [ -z "$node_3" ]; then
+				node_3="gateway"
+			fi
 
-	while true; do
-		zone_cfg_check=$($buckycli check_dns $zone_name)
-		case $zone_cfg_check in
-			"valid" )
-				echo "The zone config is valid."
-				zone_cfg=$($buckycli query_dns $zone_name)
-				node_1=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[0].name')
-				node_2=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[1].name')
-				node_3=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[2].name')
-				echo "The name of node 1 is $node_1."
-				echo "The name of node 2 is $node_2."
-				echo "The name of node 3 is $node_3."
-				break;;
-			"invalid")
-				echo "The zone name is invalid."
-				read -p "Please enter the name of node 1: " node_1 < /dev/tty
-				read -p "Please enter the name of node 2: " node_2 < /dev/tty
-				read -p "Please enter the name of node 3: " node_3 < /dev/tty
-				zone_cfg=$(create_zone_cfg $zone_name $node_ $node_2 $node_3)
-				echo $zone_cfg > /tmp/zone_cfg.json
-				$buckycli encode_dns -f /tmp/zone_cfg.json
-				encoded_cfg=$($buckycli encode_dns -f /tmp/zone_cfg.json)
-				echo $encoded_cfg
-				;;
-			* ) echo "Unknown error."
-				exit 1;;
-		esac
-	done
+			while true; do
+				read -p "Configure zone to dns or place it locally.[dns(d)/local(l)] [default: local]: " zone_mode < /dev/tty
+				if [ -z $zone_mode ]; then
+					zone_mode="local"
+				fi
+				case $zone_mode in
+					"dns" | "d" )
+						echo "You have selected dns mode."
+						while true; do
+							zone_cfg=$(create_zone_dns_cfg $zone_name $node_1 $node_2 $node_3)
+							echo $zone_cfg > /tmp/zone_cfg.json
+							$buckycli encode_dns -f /tmp/zone_cfg.json
+							encoded_cfg=$($buckycli encode_dns -f /tmp/zone_cfg.json)
+							echo "Please set the following content as the dns txt record of the domain name:"
+							echo $encoded_cfg
+
+							zone_cfg_check=$($buckycli check_dns $zone_name)
+
+							case $zone_cfg_check in
+								"valid" )
+									echo "The zone config is valid."
+									zone_cfg=$($buckycli query_dns $zone_name)
+									node_1=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[0].name')
+									node_2=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[1].name')
+									node_3=$(jq -r -n --argjson zone_cfg "$zone_cfg" '$zone_cfg.extra.etcds[2].name')
+									echo "The name of node 1 is $node_1."
+									echo "The name of node 2 is $node_2."
+									echo "The name of node 3 is $node_3."
+									break;;
+								"invalid" )
+									echo "The zone name is invalid.Please set the following content as the dns txt record of the domain name:"
+									echo $encoded_cfg
+									break;;
+								* ) echo "Unknown error."
+									exit 1;;
+							esac
+						done
+						zone_cfg_mode="dns"
+						break
+						;;
+					"local" | "l" )
+						echo "You have selected local mode."
+						zone_cfg_mode="local"
+						break
+						;;
+					* ) echo "Please answer dns or local.";;
+				esac
+			done
+			;;
+		* ) echo "Unknown error."
+			exit 1;;
+	esac
 
 	while true; do
 		echo "Please select the installation mode:"
 		echo "1. All nodes are running on the current machine."
 		echo "2. Each node is running on a separate machine."
-		read -p "Please enter the installation mode[1/2]: " install_mode < /dev/tty
+		read -p "Please enter the installation mode[1/2] [default: 1]: " install_mode < /dev/tty
+		if [ -z $install_mode ]; then
+			install_mode="1"
+		fi
 		case $install_mode in
 			"1" )
 				echo "You have selected all mode."
@@ -117,26 +171,40 @@ main() {
 
 }
 
-create_zone_cfg() {
-	zone_cfg_template="{
-                           "extra": {
-                               "etcds": [
-                                   {
-                                       "name": "$2"
-                                   },
-                                   {
-                                       "name": "$3"
-                                   },
-                                   {
-                                       "name": "$4"
-                                   }
-                               ]
-                           },
-                           "name": "$1",
-                           "type": "zone",
-                           "version": "1.0"
-                       }
-                      "
+create_zone_dns_cfg() {
+	zone_cfg_template=$(cat <<- EOF
+{
+   "extra": {
+       "etcds": [
+           {
+               "name": "$2"
+           },
+           {
+               "name": "$3"
+           },
+           {
+               "name": "$4"
+           }
+       ]
+   },
+   "name": "$1",
+   "type": "zone",
+   "version": "1.0"
+}
+EOF
+)
+    echo $zone_cfg_template
+}
+
+create_zone_local_cfg() {
+	zone_cfg_template=$(cat <<- EOF
+{
+    "zone_id" : "$1",
+    "etcd_servers":["$2","$3","$4"],
+    "etcd_data_version":0
+}
+EOF
+)
     echo $zone_cfg_template
 }
 
@@ -151,7 +219,10 @@ create_node_identity() {
 
 create_all() {
 	while true; do
-		read -p "Please enter data path: " data_path < /dev/tty
+		read -p "Please enter data path [default: ~/buckyos]: " data_path < /dev/tty
+		if [ -z $data_path ]; then
+			data_path="~/buckyos"
+		fi
 		data_path=$(eval echo "$data_path")
 		if sudo mkdir -p "$data_path" ; then
 			echo "The data path is $data_path."
@@ -179,6 +250,16 @@ create_all() {
 	ensure sudo mkdir -p "$data_path/$node_2/etcd"
 	ensure sudo mkdir -p "$data_path/$node_3/etcd"
 
+	local zone_local_cfg=""
+	local zone_local_cfg2=""
+	if [ "$zone_cfg_mode" = "local" ]; then
+		local zone_cfg=$(create_zone_local_cfg $zone_name $node_1 $node_2 $node_3)
+		ensure sudo echo -e "$zone_cfg" > "$data_path/zone_local_cfg.json"
+
+		zone_local_cfg="- $data_path/zone_local_cfg.json:/buckyos/${zone_name}_zone_config.json"
+		zone_local_cfg2="-v $data_path/zone_local_cfg.json:/buckyos/${zone_name}_zone_config.json"
+	fi
+
 	docker_compose_template=$(cat <<- EOF
 networks:
   buckyos:
@@ -203,6 +284,7 @@ services:
       - $data_path/$node_1/node_identity.toml:/buckyos/node_identity.toml
       - $data_path/$node_1/data:/buckyos/data
       - $data_path/$node_1/etcd:/buckyos/$node_1.etcd
+      $zone_local_cfg
     tty: true
     stdin_open: true
     restart: always
@@ -222,6 +304,7 @@ services:
       - $data_path/$node_2/node_identity.toml:/buckyos/node_identity.toml
       - $data_path/$node_2/data:/buckyos/data
       - $data_path/$node_2/etcd:/buckyos/$node_2.etcd
+      $zone_local_cfg
     tty: true
     stdin_open: true
     restart: always
@@ -243,6 +326,7 @@ services:
       - $data_path/$node_3/node_identity.toml:/buckyos/node_identity.toml
       - $data_path/$node_3/data:/buckyos/data
       - $data_path/$node_3/etcd:/buckyos/$node_3.etcd
+      $zone_local_cfg
     tty: true
     stdin_open: true
     restart: always
@@ -252,20 +336,20 @@ EOF
 
 	if sudo docker compose version >/dev/null 2>&1; then
 		current_dir=$(pwd)
-		ensure sudo cd "$data_path"
+		ensure cd "$data_path"
 		ensure sudo docker compose up -d
-		ensure sudo cd "$current_dir"
+		ensure cd "$current_dir"
     elif sudo docker-compose --version >/dev/null 2>&1; then
 		current_dir=$(pwd)
-		ensure sudo cd "$data_path"
+		ensure cd "$data_path"
 		ensure sudo docker-compose up -d
-		ensure sudo cd "$current_dir"
+		ensure cd "$current_dir"
     else
 	    sudo docker network create buckyos
 	    sudo docker pull harbor.mynode.site:8443/library/buckyos
-	    sudo docker run --restart=always -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined -v $data_path/$node_1/node_identity.toml:/buckyos/node_identity.toml -v "$data_path/$node_1/data":/buckyos/data -v $data_path/$node_1/etcd:/buckyos/$node_1.etcd --name $node_1 -p 139:139 -p 445:445 --network buckyos $docker_image
-	    sudo docker run --restart=always -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined -v $data_path/$node_2/node_identity.toml:/buckyos/node_identity.toml -v "$data_path/$node_2/data":/buckyos/data -v $data_path/$node_2/etcd:/buckyos/$node_2.etcd --name $node_2 --network buckyos $docker_image
-	    sudo docker run --restart=always -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined -v $data_path/$node_3/node_identity.toml:/buckyos/node_identity.toml -v "$data_path/$node_3/data":/buckyos/data -v $data_path/$node_3/etcd:/buckyos/$node_3.etcd --name $node_3 -p 2379:2379 --network buckyos $docker_image
+	    sudo docker run --restart=always -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined $zone_local_cfg2 -v $data_path/$node_1/node_identity.toml:/buckyos/node_identity.toml -v "$data_path/$node_1/data":/buckyos/data -v $data_path/$node_1/etcd:/buckyos/$node_1.etcd --name $node_1 -p 139:139 -p 445:445 --network buckyos $docker_image
+	    sudo docker run --restart=always -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined $zone_local_cfg2 -v $data_path/$node_2/node_identity.toml:/buckyos/node_identity.toml -v "$data_path/$node_2/data":/buckyos/data -v $data_path/$node_2/etcd:/buckyos/$node_2.etcd --name $node_2 --network buckyos $docker_image
+	    sudo docker run --restart=always -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined $zone_local_cfg2 -v $data_path/$node_3/node_identity.toml:/buckyos/node_identity.toml -v "$data_path/$node_3/data":/buckyos/data -v $data_path/$node_3/etcd:/buckyos/$node_3.etcd --name $node_3 -p 2379:2379 --network buckyos $docker_image
     fi
 }
 
@@ -276,19 +360,19 @@ create_node() {
 	ensure_node_dns $node_1
 	local node_1_ips=$ips
 	local node_1_ip_mode=$ip_mode
-	if [ -z "$local_node"]; then
+	if [ -z "$local_node" ]; then
 		local_node=$(is_local_host $node_1 $node_1_ips)
 	fi
 	ensure_node_dns $node_2
 	local node_2_ips=$ips
 	local node_2_ip_mode=$ip_mode
-	if [ -z "$local_node"]; then
+	if [ -z "$local_node" ]; then
 		local_node=$(is_local_host $node_2 $node_2_ips)
 	fi
 	ensure_node_dns $node_3
 	local node_3_ips=$ips
 	local node_3_ip_mode=$ip_mode
-	if [ -z "$local_node"]; then
+	if [ -z "$local_node" ]; then
 		local_node=$(is_local_host $node_3 $node_3_ips)
 	fi
 
@@ -296,7 +380,7 @@ create_node() {
 		if [ "$local_node" == "" ]; then
 			read -p "Please enter node_name[$node_1/$node_2/$node_3]: " cur_node < /dev/tty
 		else
-			read -p "Please enter node_name[$local_node]: " cur_node < /dev/tty
+			read -p "Please enter node_name[default: $local_node]: " cur_node < /dev/tty
 			if [ -z "$cur_node"]; then
 				cur_node=$local_node
 			fi
@@ -314,7 +398,10 @@ create_node() {
 	done
 
 	while true; do
-		read -p "Please enter data path: " data_path < /dev/tty
+		read -p "Please enter data path [default: ~/buckyos]: " data_path < /dev/tty
+		if [ -z $data_path ]; then
+			data_path="~/buckyos"
+		fi
 		data_path=$(eval echo "$data_path")
 		if sudo mkdir -p "$data_path" ; then
 			echo "The data path is $data_path."
@@ -329,6 +416,14 @@ create_node() {
 	ensure sudo mkdir -p "$data_path/$cur_node/data"
 	ensure sudo mkdir -p "$data_path/$cur_node/data/gv0"
 	ensure sudo mkdir -p "$data_path/$cur_node/etcd"
+
+	local zone_local_cfg=""
+	if [ "$zone_cfg_mode" = "local" ]; then
+		local zone_cfg=$(create_zone_local_cfg $zone_name $node_1 $node_2 $node_3)
+		ensure sudo echo -e "$zone_cfg" > "$data_path/zone_local_cfg.json"
+
+		zone_local_cfg="-v $data_path/zone_local_cfg.json:/buckyos/${zone_name}_zone_config.json"
+	fi
 
 	local node_1_host=""
 	if [[ "$node_1_ip_mode" == "manual" && "$cur_node" != "$node_1" ]]; then
@@ -346,8 +441,8 @@ create_node() {
 	    node_3_host="--add-host $node_3:${ip_list[0]}"
 	fi
 	ensure sudo docker pull $docker_image
-# 	echo "sudo docker run -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined --restart=always $node_1_host $node_2_host $node_3_host -v "$data_path/$cur_node/node_identity.toml":/buckyos/node_identity.toml -v "$data_path/$cur_node/data":/buckyos/data -v $data_path/$cur_node/etcd:/buckyos/$cur_node.etcd  --name buckyos -h $cur_node -p 24008:24008 -p 24007:24007 -p 49152-60999:49152-60999 -p 139:139 -p 445:445 -p 2379:2379 -p 2380:2380 $docker_image"
-	ensure sudo docker run -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined --restart=always $node_1_host $node_2_host $node_3_host -v "$data_path/$cur_node/node_identity.toml":/buckyos/node_identity.toml -v "$data_path/$cur_node/data":/buckyos/data -v $data_path/$cur_node/etcd:/buckyos/$cur_node.etcd  --name buckyos -h $cur_node -p 24008:24008 -p 24007:24007 -p 49152-60999:49152-60999 -p 139:139 -p 445:445 -p 2379:2379 -p 2380:2380 $docker_image
+ 	#echo "sudo docker run -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined --restart=always $node_1_host $node_2_host $node_3_host $zone_local_cfg -v "$data_path/$cur_node/node_identity.toml":/buckyos/node_identity.toml -v "$data_path/$cur_node/data":/buckyos/data -v $data_path/$cur_node/etcd:/buckyos/$cur_node.etcd  --name buckyos -h $cur_node -p 24008:24008 -p 24007:24007 -p 49152-49162:49152-49162 -p 139:139 -p 445:445 -p 2379:2379 -p 2380:2380 $docker_image"
+	ensure sudo docker run -d --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor=unconfined --restart=always $node_1_host $node_2_host $node_3_host $zone_local_cfg -v "$data_path/$cur_node/node_identity.toml":/buckyos/node_identity.toml -v "$data_path/$cur_node/data":/buckyos/data -v $data_path/$cur_node/etcd:/buckyos/$cur_node.etcd  --name buckyos -h $cur_node -p 24008:24008 -p 24007:24007 -p 49152-49162:49152-49162 -p 139:139 -p 445:445 -p 2379:2379 -p 2380:2380 $docker_image
 }
 
 ensure_node_dns() {
@@ -357,7 +452,10 @@ ensure_node_dns() {
 	ips=$(nslookup $node_name | grep 'Address:' | tail -n +2 | awk '{print $2}')
 	if [ -z "$ips" ]; then
 		while true; do
-			read -p "The domain $node_name can't resolve IP address.Do you want to configure dns or enter the ip address manually?[dns(d)/manual(m)]: " dns_mode < /dev/tty
+			read -p "The domain $node_name can't resolve IP address.Do you want to configure dns or enter the ip address manually?[dns(d)/manual(m)] [default: manual]: " dns_mode < /dev/tty
+			if [ -z $dns_mode ]; then
+				dns_mode="manual"
+			fi
 			case $dns_mode in
 				"dns" | "d" )
 					while true; do
@@ -557,7 +655,7 @@ EOF
 
 ensure_etcd_cluster_health() {
 	while true; do
-		cluster_status=$($buckycli check_etcd_cluster)
+		local cluster_status=$($buckycli check_etcd_cluster)
 		if [ "$cluster_status" = "The etcd cluster is healthy" ]; then
 			echo "The etcd cluster is healthy."
 			break
@@ -582,7 +680,7 @@ check_cmd() {
 # will immediately terminate with an error showing the failing
 # command.
 ensure() {
-    if ! "$@"; then err "command failed: $*"; fi
+    if ! "$@"; then echo "command failed: $*"; fi
 }
 
 # This is just for indicating that commands' results are being
