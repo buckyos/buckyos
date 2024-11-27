3bf14c33690dc4bac9db558761112609bfa88742
weiqiushi <weiqiushi@buckyos.com>
2024-11-27 15:43:23 +0800

    install on windows

--- src/scripts/install.py
+++ src/scripts/install.py
@@ -4,19 +4,28 @@ import platform
 
 src_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
+install_root_dir = ""
+
+if platform.system() == "Windows":
+    install_root_dir = os.path.join(os.path.expandvars("%AppData%"), "buckyos")
+elif platform.system() == "Linux":
+    install_root_dir = "/opt/buckyos"
+
 def install():
-    if platform.system() == "Windows":
-        print("Windows not support install")
+    if install_root_dir == "":
+        print("Unknown platform, not support install, skip.")
         return
     # if /opt/buckyos not exist, copy rootfs to /opt/buckyos
-    if not os.path.exists("/opt/buckyos"):
-        print('copying rootfs to /opt/buckyos')
-        shutil.copytree(os.path.join(src_dir, "rootfs"), "/opt/buckyos")
+    print(f"installing to {install_root_dir}")
+    if not os.path.exists(install_root_dir):
+        print(f'copying rootfs to {install_root_dir}')
+        shutil.copytree(os.path.join(src_dir, "rootfs"), install_root_dir)
     else:
-        print('updating files in /opt/buckyos/bin')
-        shutil.rmtree("/opt/buckyos/bin")
+        bin_dir = os.path.join(install_root_dir, "bin")
+        print(f'updating files in {bin_dir}')
+        shutil.rmtree(bin_dir)
         #just update bin
-        shutil.copytree(os.path.join(src_dir, "rootfs/bin"), "/opt/buckyos/bin")
+        shutil.copytree(os.path.join(src_dir, "rootfs/bin"), bin_dir)
 
 if __name__ == "__main__":
     install()
