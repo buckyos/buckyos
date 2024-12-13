7c96ff12e4779417e1785bcd8fd9637a01b0d9e8
alexsunxl <alexsunxl@163.com>
2024-12-13 15:05:31 +0800

    Add task_manager implement

---
+++ src/kernel/task_manager/src/task.rs
@@ -0,0 +1,42 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[derive(Serialize, Deserialize)]
+pub enum TaskStatus {
+    Running,
+    Paused,
+    Completed,
+}
+
+impl ToString for TaskStatus {
+    fn to_string(&self) -> String {
+        match *self {
+            TaskStatus::Running => "Running".to_string(),
+            TaskStatus::Paused => "Paused".to_string(),
+            TaskStatus::Completed => "Completed".to_string(),
+        }
+    }
+}
+
+impl FromStr for TaskStatus {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Running" => Ok(TaskStatus::Running),
+            "Paused" => Ok(TaskStatus::Paused),
+            "Completed" => Ok(TaskStatus::Completed),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Task {
+    pub id: String,
+    pub name: String,
+    pub status: TaskStatus,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
