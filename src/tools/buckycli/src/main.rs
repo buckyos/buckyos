033c4ca2f335cc6a35cd346c371614f848b1a3f1
Song <glen0125@foxmail.com>
2024-12-24 15:48:54 +0800

    Fix pkg pack tool

--- src/tools/buckycli/src/main.rs
+++ src/tools/buckycli/src/main.rs
@@ -1,6 +1,10 @@
 extern crate core;
-
+mod package_cmd;
+use buckyos_kit::get_buckyos_system_etc_dir;
 use clap::{value_parser, Arg, ArgMatches, Command};
+use jsonwebtoken::EncodingKey;
+use name_lib::{decode_json_from_jwt_with_default_pk, DeviceConfig};
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
@@ -8,61 +12,50 @@ use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::process::Command as SystemCommand;
 use std::time::{SystemTime, UNIX_EPOCH};
-use jsonwebtoken::EncodingKey;
-use serde::Deserialize;
-use buckyos_kit::get_buckyos_system_etc_dir;
-use name_lib::{decode_json_from_jwt_with_default_pk, DeviceConfig};
 
 const CONFIG_FILE: &str = "~/.buckycli/config";
 
 #[derive(Deserialize, Debug)]
 struct NodeIdentityConfig {
-    zone_name: String,// $name.buckyos.org or did:ens:$name
+    zone_name: String,                        // $name.buckyos.org or did:ens:$name
     owner_public_key: jsonwebtoken::jwk::Jwk, //owner is zone_owner
-    owner_name:String,//owner's name
-    device_doc_jwt:String,//device document,jwt string,siged by owner
-    zone_nonce:String,// random string, is default password of some service
-    //device_private_key: ,storage in partical file
+    owner_name: String,                       //owner's name
+    device_doc_jwt: String,                   //device document,jwt string,siged by owner
+    zone_nonce: String,                       // random string, is default password of some service
+                                              //device_private_key: ,storage in partical file
 }
 
 fn load_identity_config(node_id: &str) -> Result<NodeIdentityConfig, String> {
     //load ./node_identity.toml for debug
     //load from /opt/buckyos/etc/node_identity.toml
-    let mut file_path = PathBuf::from(format!("{}_identity.toml",node_id));
+    let mut file_path = PathBuf::from(format!("{}_identity.toml", node_id));
     let path = Path::new(&file_path);
     if !path.exists() {
         let etc_dir = get_buckyos_system_etc_dir();
-        file_path = etc_dir.join(format!("{}_identity.toml",node_id));
+        file_path = etc_dir.join(format!("{}_identity.toml", node_id));
     }
 
-    let contents = std::fs::read_to_string(file_path.clone()).map_err(|err| {
-        format!("read node identity config failed! {}", err)
-    })?;
+    let contents = std::fs::read_to_string(file_path.clone())
+        .map_err(|err| format!("read node identity config failed! {}", err))?;
 
-    let config: NodeIdentityConfig = toml::from_str(&contents).map_err(|err| {
-        format!(
-            "Failed to parse NodeIdentityConfig TOML: {}",
-            err
-        )
-    })?;
+    let config: NodeIdentityConfig = toml::from_str(&contents)
+        .map_err(|err| format!("Failed to parse NodeIdentityConfig TOML: {}", err))?;
 
     Ok(config)
 }
 
 fn load_device_private_key(node_id: &str) -> Result<EncodingKey, String> {
-    let mut file_path = format!("{}_private_key.pem",node_id);
+    let mut file_path = format!("{}_private_key.pem", node_id);
     let path = Path::new(file_path.as_str());
     if !path.exists() {
         let etc_dir = get_buckyos_system_etc_dir();
-        file_path = format!("{}/{}_private_key.pem",etc_dir.to_string_lossy(),node_id);
+        file_path = format!("{}/{}_private_key.pem", etc_dir.to_string_lossy(), node_id);
     }
-    let private_key = std::fs::read_to_string(file_path.clone()).map_err(|err| {
-        format!("read device private key failed! {}", err)
-    })?;
+    let private_key = std::fs::read_to_string(file_path.clone())
+        .map_err(|err| format!("read device private key failed! {}", err))?;
 
-    let private_key: EncodingKey = EncodingKey::from_ed_pem(private_key.as_bytes()).map_err(|err| {
-        format!("parse device private key failed! {}", err)
-    })?;
+    let private_key: EncodingKey = EncodingKey::from_ed_pem(private_key.as_bytes())
+        .map_err(|err| format!("parse device private key failed! {}", err))?;
 
     Ok(private_key)
 }
@@ -80,10 +73,7 @@ async fn main() -> std::result::Result<(), String> {
                 .help("This node's id")
                 .required(false),
         )
-        .subcommand(
-            Command::new("create_token")
-                .about("Create device session token"),
-        )
+        .subcommand(Command::new("create_token").about("Create device session token"))
         .subcommand(Command::new("version").about("buckyos version"))
         // .arg(
         //     Arg::new("snapshot")
@@ -117,7 +107,10 @@ async fn main() -> std::result::Result<(), String> {
         }
     };
 
-    let device_doc_json = match decode_json_from_jwt_with_default_pk(&node_config.device_doc_jwt, &node_config.owner_public_key) {
+    let device_doc_json = match decode_json_from_jwt_with_default_pk(
+        &node_config.device_doc_jwt,
+        &node_config.owner_public_key,
+    ) {
         Ok(device_doc_json) => device_doc_json,
         Err(e) => {
             println!("decode device doc from jwt failed!");
@@ -133,27 +126,27 @@ async fn main() -> std::result::Result<(), String> {
         }
     };
 
-
     match matches.subcommand() {
         Some(("create_token", matches)) => {
             let now = SystemTime::now();
-            let since_the_epoch = now.duration_since(UNIX_EPOCH)
-                .expect("Time went backwards");
+            let since_the_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
             let timestamp = since_the_epoch.as_secs();
             let device_session_token = kRPC::RPCSessionToken {
-                token_type : kRPC::RPCSessionTokenType::JWT,
-                nonce : None,
-                userid : Some(device_doc.name.clone()),
-                appid:Some("kernel".to_string()),
-                exp:Some(timestamp + 3600*24*7),
-                iss:Some(device_doc.name.clone()),
-                token:None,
+                token_type: kRPC::RPCSessionTokenType::JWT,
+                nonce: None,
+                userid: Some(device_doc.name.clone()),
+                appid: Some("kernel".to_string()),
+                exp: Some(timestamp + 3600 * 24 * 7),
+                iss: Some(device_doc.name.clone()),
+                token: None,
             };
 
-            let device_session_token_jwt = device_session_token.generate_jwt(Some(device_doc.did.clone()),&device_private_key)
+            let device_session_token_jwt = device_session_token
+                .generate_jwt(Some(device_doc.did.clone()), &device_private_key)
                 .map_err(|err| {
                     println!("generate device session token failed! {}", err);
-                    return String::from("generate device session token failed!");})?;
+                    return String::from("generate device session token failed!");
+                })?;
             println!("{}", device_session_token_jwt)
         }
         Some(("version", _)) => {
--- src/tools/buckycli/src/package_cmd.rs
+++ src/tools/buckycli/src/package_cmd.rs
@@ -1,3 +1,4 @@
+use core::hash;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use reqwest::header::{HeaderMap, HeaderValue};
@@ -7,8 +8,8 @@ use serde_json;
 use sha2::{Digest, Sha256};
 use std::fs;
 use std::fs::File;
-use std::io::{self, Read};
-use std::path::Path;
+use std::io::{self, BufReader, Read};
+use std::path::{Path, PathBuf};
 use tar::Builder;
 use toml::Value;
 
@@ -21,12 +22,44 @@ struct UploadPackageMeta {
     sha256: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct PackResult {
+    pkg_id: String,
+    version: String,
+    vendor_did: String,
+    target_file_path: PathBuf, // tarball path
+}
+
 #[derive(Deserialize)]
 struct ServerError {
     error: String,
 }
 
-pub async fn pack(path: &str) -> Result<(), String> {
+/*
+meta.json
+{
+    "name" : "Home Station",
+    "description" : "Home Station",
+    "vendor_did" : "did:bns:buckyos",
+    "pkg_id" : "home-station",
+    "version" : "0.1.0",
+    "pkg_list" : {
+        "amd64_docker_image" : {
+            "pkg_id":"home-station-x86-img",
+            "docker_image_name":"filebrowser/filebrowser:s6"
+        },
+        "aarch64_docker_image" : {
+            "pkg_id":"home-station-arm64-img",
+            "docker_image_name":"filebrowser/filebrowser:s6"
+        },
+        "web_pages" :{
+            "pkg_id" : "home-station-web-page"
+        }
+    }
+}
+ */
+
+pub async fn pack(path: &str) -> Result<PackResult, String> {
     let path = Path::new(path);
     if !path.exists() {
         eprintln!("Error: Specified directory does not exist");
@@ -36,70 +69,55 @@ pub async fn pack(path: &str) -> Result<(), String> {
     println!("Starting the packing process for directory: {:?}", path);
 
     // 检查目录
-    let package_toml_path = path.join("package.toml");
-    if !package_toml_path.exists() {
-        eprintln!("Error: package.toml not found in the specified directory");
-        return Err("package.toml not found in the specified directory".to_string());
+    let meta_path = path.join("meta.json");
+    if !meta_path.exists() {
+        eprintln!("Error: meta.json not found in directory");
+        return Err("meta.json not found in directory".to_string());
     }
 
     let items: Vec<_> = fs::read_dir(path)
         .map_err(|e| format!("Error: Failed to read directory: {}", e.to_string()))?
         .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.file_name() != "target")
         .collect();
 
     if items.len() <= 1 {
-        eprintln!("Error: No files or folders found to package (excluding target)");
-        return Err("No files or folders found to package (excluding target)".to_string());
+        eprintln!("Error: No files or folders found to pack");
+        return Err("No files or folders found to pack".to_string());
     }
 
-    println!("Found {} items to package (excluding target)", items.len());
+    println!("Found {} items to pack", items.len());
 
-    // 解析 package.toml
-    let package_toml_content = fs::read_to_string(&package_toml_path)
-        .map_err(|err| format!("Error: Failed to read package.toml: {}", err.to_string()))?;
-    let package_data: Value = toml::from_str(&package_toml_content)
-        .map_err(|err| format!("Error: Failed to parse package.toml: {}", err.to_string()))?;
+    // 解析 meta.json
+    let meta_content = fs::read_to_string(&meta_path)
+        .map_err(|err| format!("Error: Failed to read meta.json: {}", err.to_string()))?;
+    let meta_data: Value = serde_json::from_str(&meta_content)
+        .map_err(|err| format!("Error: Failed to parse meta.json: {}", err.to_string()))?;
 
-    let name = package_data
-        .get("package")
-        .and_then(|pkg| pkg.get("name"))
+    let pkg_id = meta_data
+        .get("pkg_id")
         .and_then(Value::as_str)
-        .ok_or_else(|| format!("Error: Package name missing in package.toml"))?;
+        .ok_or_else(|| format!("Error: pkg_id missing in meta.json"))?;
 
-    let version = package_data
-        .get("package")
-        .and_then(|pkg| pkg.get("version"))
+    let version = meta_data
+        .get("version")
         .and_then(Value::as_str)
-        .ok_or_else(|| format!("Error: Package version missing in package.toml"))?;
+        .ok_or_else(|| format!("Error: version missing in meta.json"))?;
 
-    let author = package_data
-        .get("package")
-        .and_then(|pkg| pkg.get("author"))
+    let vendor_did = meta_data
+        .get("vendor_did")
         .and_then(Value::as_str)
-        .unwrap_or("Unknown");
+        .ok_or_else(|| format!("Error: vendor_did missing in meta.json"))?;
 
-    let default_dependencies = Value::Table(Default::default());
-    let dependencies = package_data
-        .get("dependencies")
-        .unwrap_or(&default_dependencies);
-
+    // TODO dependencies？
     println!(
-        "Parsed package.toml: name = {}, version = {}, author = {}",
-        name, version, author
+        "Parsed meta.json: pkg_id = {}, version = {}, vendor_did = {}",
+        pkg_id, version, vendor_did
     );
-    println!("Dependencies: {:?}", dependencies);
 
     // 创建 tarball
-    let target_dir = path.join("target");
-    fs::create_dir_all(&target_dir).map_err(|err| {
-        format!(
-            "Error: Failed to create target directory for package: {}",
-            err.to_string()
-        )
-    })?;
-    let tarball_name = format!("{}-{}.bkz", name, version);
-    let tarball_path = target_dir.join(&tarball_name);
+    let parent_dir = path.parent().unwrap();
+    let tarball_name = format!("{}-{}.bkz", pkg_id, version);
+    let tarball_path = parent_dir.join(&tarball_name);
 
     let tar_gz = File::create(&tarball_path)
         .map_err(|err| format!("Error: Failed to create package file: {}", err.to_string()))?;
@@ -116,11 +134,6 @@ pub async fn pack(path: &str) -> Result<(), String> {
             let entry = entry?;
             let path = entry.path();
             let name = path.strip_prefix(base).unwrap();
-
-            // 排除 target 目录
-            if name.starts_with("target") {
-                continue;
-            }
 
             if path.is_dir() {
                 tar.append_dir(name, &path)?;
@@ -148,149 +161,128 @@ pub async fn pack(path: &str) -> Result<(), String> {
 
     println!(
         "Package {} version {} by {} has been packed successfully.",
-        name, version, author
+        pkg_id, version, vendor_did
     );
     println!("Package created at: {:?}", tarball_path);
 
-    Ok(())
+    let pack_ret = PackResult {
+        pkg_id: pkg_id.to_string(),
+        version: version.to_string(),
+        vendor_did: vendor_did.to_string(),
+        target_file_path: tarball_path,
+    };
+
+    Ok(pack_ret)
+}
+
+fn calculate_file_hash(file_path: &str) -> Result<String, String> {
+    let file = File::open(file_path).map_err(|err| {
+        format!(
+            "Error: Failed to open package file {}: {}",
+            file_path,
+            err.to_string()
+        )
+    })?;
+    let mut reader = BufReader::new(file);
+    let mut hasher = Sha256::new();
+    let mut buffer = [0u8; 10 * 1024 * 1024]; // 10MB 缓冲区
+
+    loop {
+        let bytes_read = reader.read(&mut buffer).map_err(|err| {
+            format!(
+                "Error: Failed to read package file {} for hashing: {}",
+                file_path,
+                err.to_string()
+            )
+        })?;
+        if bytes_read == 0 {
+            break;
+        }
+        hasher.update(&buffer[..bytes_read]);
+    }
+
+    let hash = hasher.finalize();
+    let hash_hex = format!("{:x}", hash);
+    Ok(hash_hex)
 }
 
 pub async fn publish(path: &str, server: &str) -> Result<(), String> {
-    let path = Path::new(path);
+    let pack_ret = pack(path).await?;
 
-    // 解析 package.toml
-    let package_toml_path = path.join("package.toml");
-    let package_toml_content = fs::read_to_string(&package_toml_path)
-        .map_err(|err| format!("Error: Failed to read package.toml: {}", err.to_string()))?;
-    let package_data: Value = toml::from_str(&package_toml_content)
-        .map_err(|e| format!("Error: Failed to parse package.toml: {}", e.to_string()))?;
+    let pack_file_path = pack_ret.target_file_path.clone();
 
-    let name: &str = package_data
-        .get("package")
-        .and_then(|pkg| pkg.get("name"))
-        .and_then(Value::as_str)
-        .ok_or_else(|| format!("Error: Package name missing in package.toml"))?;
-
-    let version = package_data
-        .get("package")
-        .and_then(|pkg| pkg.get("version"))
-        .and_then(Value::as_str)
-        .ok_or_else(|| {
-            format!(
-                "Error: Package version missing in package.toml for package {}",
-                name
-            )
-        })?;
-
-    let author = package_data
-        .get("package")
-        .and_then(|pkg| pkg.get("author"))
-        .and_then(Value::as_str)
-        .unwrap_or("Unknown");
-
-    let default_dependencies = Value::Table(Default::default());
-    let dependencies = package_data
-        .get("dependencies")
-        .unwrap_or(&default_dependencies);
-
-    println!(
-        "Parsed package.toml: name = {}, version = {}, author = {}",
-        name, version, author
-    );
-    println!("Dependencies: {:?}", dependencies);
-
-    // 确保 tarball 存在
-    let tarball_name = format!("{}-{}.bkz", name, version);
-    let tarball_path = path.join("target").join(&tarball_name);
-
-    if !tarball_path.exists() {
-        eprintln!("Error: Package file {} not found", tarball_name);
-        return Err(format!("Package file {} not found", tarball_name));
+    if !pack_file_path.exists() {
+        eprintln!("Error: Package file {} not found", pack_file_path.display());
+        return Err(format!(
+            "Package file {} not found",
+            pack_file_path.display()
+        ));
     }
 
-    // 计算 tarball 的 sha256
-    let mut file = File::open(&tarball_path).map_err(|err| {
-        format!(
-            "Error: Failed to open package file {} for reading: {}",
-            tarball_name,
-            err.to_string()
-        )
-    })?;
-    let mut hasher = Sha256::new();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).map_err(|err| {
-        format!(
-            "Error: Failed to read package file {} for hashing: {}",
-            tarball_name,
-            err.to_string()
-        )
-    })?;
-    hasher.update(&buffer);
-    let hash = hasher.finalize();
-    let hash_hex = format!("{:x}", hash);
+    let hash_hex = calculate_file_hash(pack_file_path.to_str().unwrap())?;
 
     // 创建元数据
-    let metadata = UploadPackageMeta {
-        name: name.to_string(),
-        version: version.to_string(),
-        author: author.to_string(),
-        deps: dependencies.clone(),
-        sha256: hash_hex,
-    };
+    // let metadata = UploadPackageMeta {
+    //     name: name.to_string(),
+    //     version: version.to_string(),
+    //     author: author.to_string(),
+    //     deps: dependencies.clone(),
+    //     sha256: hash_hex,
+    // };
 
-    // 将元数据序列化为 JSON
-    let metadata_json = serde_json::to_string(&metadata).map_err(|e| {
-        format!(
-            "Error: Failed to serialize package metadata to JSON: {}",
-            e.to_string()
-        )
-    })?;
+    // // 将元数据序列化为 JSON
+    // let metadata_json = serde_json::to_string(&metadata).map_err(|e| {
+    //     format!(
+    //         "Error: Failed to serialize package metadata to JSON: {}",
+    //         e.to_string()
+    //     )
+    // })?;
 
-    // 可读打印元数据
-    println!("Package metadata: {:#?}", metadata);
-    // 上传文件
-    let client = Client::new();
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        "Bkz-Upload-Metadata",
-        HeaderValue::from_str(&metadata_json).unwrap(),
-    );
+    // // 可读打印元数据
+    // println!("Package metadata: {:#?}", metadata);
+    // // 上传文件
+    // let client = Client::new();
+    // let mut headers = HeaderMap::new();
+    // headers.insert(
+    //     "Bkz-Upload-Metadata",
+    //     HeaderValue::from_str(&metadata_json).unwrap(),
+    // );
 
-    let response = client
-        .post(server)
-        .headers(headers)
-        .body(buffer)
-        .send()
-        .await
-        .map_err(|e| format!("Error: Failed to send package to server: {}", e.to_string()))?;
+    // let response = client
+    //     .post(server)
+    //     .headers(headers)
+    //     .body(buffer)
+    //     .send()
+    //     .await
+    //     .map_err(|e| format!("Error: Failed to send package to server: {}", e.to_string()))?;
 
-    if response.status().is_success() {
-        println!(
-            "Package {} version {} by {} has been published successfully.",
-            name, version, author
-        );
-    } else {
-        let status = response.status();
-        let error_message = response.text().await.unwrap_or_else(|e| {
-            eprintln!("Failed to read error message: {}", e);
-            "No error message returned".to_string()
-        });
+    // if response.status().is_success() {
+    //     println!(
+    //         "Package {} version {} by {} has been published successfully.",
+    //         name, version, author
+    //     );
+    // } else {
+    //     let status = response.status();
+    //     let error_message = response.text().await.unwrap_or_else(|e| {
+    //         eprintln!("Failed to read error message: {}", e);
+    //         "No error message returned".to_string()
+    //     });
 
-        // 尝试解析 JSON 错误消息
-        if let Ok(server_error) = serde_json::from_str::<ServerError>(&error_message) {
-            eprintln!(
-                "Failed to publish package: {:?}. Error message: {}",
-                status, server_error.error
-            );
-            return Err(server_error.error);
-        } else {
-            eprintln!(
-                "Failed to publish package: {:?}. Error message: {}",
-                status, error_message
-            );
-            return Err(error_message);
-        }
-    }
+    //     // 尝试解析 JSON 错误消息
+    //     if let Ok(server_error) = serde_json::from_str::<ServerError>(&error_message) {
+    //         eprintln!(
+    //             "Failed to publish package: {:?}. Error message: {}",
+    //             status, server_error.error
+    //         );
+    //         return Err(server_error.error);
+    //     } else {
+    //         eprintln!(
+    //             "Failed to publish package: {:?}. Error message: {}",
+    //             status, error_message
+    //         );
+    //         return Err(error_message);
+    //     }
+    // }
 
     Ok(())
 }
