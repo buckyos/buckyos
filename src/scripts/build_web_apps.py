888a8295355caaac71badf60fb3e640cc80e6969
weiqiushi <weiqiushi@buckyos.com>
2025-02-05 19:19:12 +0800

    Adjust web app build script

--- src/scripts/build_web_apps.py
+++ src/scripts/build_web_apps.py
@@ -6,7 +6,7 @@ src_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 def build(dir):
     print(f'building at {dir}')
     work_dir = os.path.join(src_dir, dir)
-    subprocess.run(f'pnpm update buckyos && pnpm install && pnpm run build', shell=True, cwd=work_dir, check=True)
+    subprocess.run(f'pnpm install && pnpm update buckyos &&pnpm run build', shell=True, cwd=work_dir, check=True)
 
 def build_web_apps():
     print('will build web apps')
