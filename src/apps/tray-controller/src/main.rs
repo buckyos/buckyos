9a4f0c1162537f97a3938c349ff3e16b29cfd451
zhangzhen <zhangzhen@buckyos.com>
2025-02-19 16:08:54 +0800

    ensure that only one instance is started

--- src/apps/tray-controller/src/main.rs
+++ src/apps/tray-controller/src/main.rs
@@ -1,6 +1,13 @@
 #![windows_subsystem = "windows"]
 
+use std::fs::OpenOptions;
+use std::process;
+
 use buckyos_kit::init_logging;
+use fs2::FileExt;
+
+#[cfg(windows)]
+use std::os::windows::fs::OpenOptionsExt;
 
 mod ffi_extern;
 
@@ -11,8 +18,26 @@ extern "C" {
 fn main() {
     init_logging("tray-controller");
 
-    log::info!("buckyos tray-controller started.");
-    unsafe {
-        entry();
+    let mut file = OpenOptions::new();
+    file.read(true).write(true).create(true);
+
+    #[cfg(windows)]
+    file.share_mode(0);
+
+    let file = file
+        .open(std::env::temp_dir().join("buckyos-tray-controller.lock"))
+        .expect("open lock-file failed.");
+
+    match file.try_lock_exclusive() {
+        Ok(_) => {
+            log::info!("buckyos tray-controller started.");
+            unsafe {
+                entry();
+            }
+        }
+        Err(_) => {
+            log::info!("Another tray-controller is already running.");
+            process::exit(1);
+        }
     }
 }
