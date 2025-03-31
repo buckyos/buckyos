9da22c1c17ac7279bc1c0cd8a48805d151151655
liyaxing <liyaxing@buckyos.com>
2025-03-31 20:52:26 +0800

    Add generic storage impl with other hash methods

--- src/components/ndn-lib/src/path_object_map/storage.rs
+++ src/components/ndn-lib/src/path_object_map/storage.rs
@@ -1,73 +1,85 @@
-use crate::{NdnError, NdnResult, ObjId};
-use hash_db::{Hasher};
-use super::hash::{Blake2s256Hasher, Keccak256Hasher, Sha512Hasher, Sha256Hasher};
+use super::hash::{Blake2s256Hasher, Keccak256Hasher, Sha256Hasher, Sha512Hasher};
+use crate::{HashMethod, NdnError, NdnResult, ObjId};
+use hash_db::Hasher;
 use memory_db::{HashKey, MemoryDB};
+use reference_trie::{RefTrieDB, RefTrieDBMut, ReferenceNodeCodec};
 use std::sync::{Arc, RwLock};
-use reference_trie::{RefTrieDBMut, RefTrieDB, ReferenceNodeCodec};
 use trie_db::proof::generate_proof;
-use trie_db::{TrieMut, Trie, TrieLayout, NodeCodec, Value};
+use trie_db::{NodeCodec, Trie, TrieLayout, TrieMut, Value};
 
 #[async_trait::async_trait]
-pub trait DBStorage: Send + Sync {
-    type Hasher: Hasher;
+pub trait PathObjectMapInnerStorage: Send + Sync {
 
     async fn put(&self, key: &[u8], value: &[u8]) -> NdnResult<()>;
     async fn get(&self, key: &[u8]) -> NdnResult<Option<Vec<u8>>>;
     async fn remove(&self, key: &[u8]) -> NdnResult<Option<Vec<u8>>>;
     async fn commit(&self) -> NdnResult<()>;
-    async fn root(&self) -> <Self::Hasher as Hasher>::Out;
+    async fn root(&self) -> Vec<u8>;
     async fn generate_proof(&self, key: &[u8]) -> NdnResult<Vec<Vec<u8>>>;
 }
 
+pub type PathObjectMapInnerStorageRef = Arc<Box<dyn PathObjectMapInnerStorage>>;
 
-type Sha256DB = MemoryDB<Sha256Hasher, HashKey<Sha256Hasher>, Vec<u8>>;
-type Sha512DB = MemoryDB<Sha512Hasher, HashKey<Sha512Hasher>, Vec<u8>>;
-type Blake2s256DB = MemoryDB<Blake2s256Hasher, HashKey<Blake2s256Hasher>, Vec<u8>>;
-type Keccak256DB = MemoryDB<Keccak256Hasher, HashKey<Keccak256Hasher>, Vec<u8>>;
+type GenericMemoryDB<H> = MemoryDB<H, HashKey<H>, Vec<u8>>;
+type Sha256DB = GenericMemoryDB<Sha256Hasher>;
+type Sha512DB = GenericMemoryDB<Sha512Hasher>;
+type Blake2s256DB = GenericMemoryDB<Blake2s256Hasher>;
+type Keccak256DB = GenericMemoryDB<Keccak256Hasher>;
 
+pub struct GenericLayout<H: Hasher>(std::marker::PhantomData<H>);
 
-pub struct Sha256Layout;
-
-impl TrieLayout for Sha256Layout {
+impl<H: Hasher> TrieLayout for GenericLayout<H> {
     const USE_EXTENSION: bool = true;
     const ALLOW_EMPTY: bool = false;
     const MAX_INLINE_VALUE: Option<u32> = None;
 
-    type Hash = Sha256Hasher; 
-    type Codec = ReferenceNodeCodec<Sha256Hasher>;
+    type Hash = H;
+    type Codec = ReferenceNodeCodec<H>;
 }
 
-type Sha256TrieDB<'a, 'cache> = trie_db::TrieDB<'a, 'cache, Sha256Layout>;
-type Sha256TrieDBBuilder<'a, 'cache> = trie_db::TrieDBBuilder<'a, 'cache, Sha256Layout>;
-type Sha256TrieDBMut<'a> = trie_db::TrieDBMut<'a, Sha256Layout>;
-type Sha256TrieDBMutBuilder<'a> = trie_db::TrieDBMutBuilder<'a, Sha256Layout>;
+type GenericTrieDB<'a, 'cache, H> = trie_db::TrieDB<'a, 'cache, GenericLayout<H>>;
+type GenericTrieDBBuilder<'a, 'cache, H> = trie_db::TrieDBBuilder<'a, 'cache, GenericLayout<H>>;
+type GenericTrieDBMut<'a, H> = trie_db::TrieDBMut<'a, GenericLayout<H>>;
+type GenericTrieDBMutBuilder<'a, H> = trie_db::TrieDBMutBuilder<'a, GenericLayout<H>>;
 
-pub struct Sha256MemoryStorage {
-    db: Arc<RwLock<Sha256DB>>,
-    root: Arc<RwLock<<Sha256Hasher as Hasher>::Out>>,
+pub struct GenericMemoryStorage<H: Hasher> {
+    db: Arc<RwLock<GenericMemoryDB<H>>>,
+    root: Arc<RwLock<<H as Hasher>::Out>>,
 }
 
-#[async_trait::async_trait ]
-impl DBStorage for Sha256MemoryStorage {
-    type Hasher = Sha256Hasher;
+impl<H: Hasher> GenericMemoryStorage<H> {
+    pub fn new() -> Self {
+        let root = Default::default();
+        let db = Arc::new(RwLock::new(GenericMemoryDB::<H>::default()));
+        let root = Arc::new(RwLock::new(root));
+        Self { db, root }
+    }
+}
+
+#[async_trait::async_trait]
+impl<H> PathObjectMapInnerStorage for GenericMemoryStorage<H>
+where
+    H: Hasher + Send + Sync + 'static,
+    H::Out: Send + Sync + 'static,
+{
 
     async fn put(&self, key: &[u8], value: &[u8]) -> NdnResult<()> {
         let mut db_write = self.db.write().unwrap();
         let mut root = self.root.write().unwrap();
-        let mut trie = Sha256TrieDBMutBuilder::new(&mut *db_write, &mut root).build();
+        let mut trie = GenericTrieDBMutBuilder::new(&mut *db_write, &mut root).build();
         trie.insert(key, value).map_err(|e| {
             let msg = format!("Failed to insert key-value pair: {:?}", e);
             error!("{}", msg);
             NdnError::DbError(msg)
         })?;
-    
+
         Ok(())
     }
 
     async fn get(&self, key: &[u8]) -> NdnResult<Option<Vec<u8>>> {
         let db_read = self.db.read().unwrap();
         let root = self.root.read().unwrap();
-        let trie = Sha256TrieDBBuilder::new(&*db_read, &root).build();
+        let trie = GenericTrieDBBuilder::new(&*db_read, &root).build();
         let value = trie.get(key).map_err(|e| {
             let msg = format!("Failed to get value for key: {:?}", e);
             error!("{}", msg);
@@ -80,7 +92,7 @@ impl DBStorage for Sha256MemoryStorage {
     async fn remove(&self, key: &[u8]) -> NdnResult<Option<Vec<u8>>> {
         let mut db_write = self.db.write().unwrap();
         let mut root = self.root.write().unwrap();
-        let mut trie = Sha256TrieDBMutBuilder::new(&mut *db_write, &mut root).build();
+        let mut trie = GenericTrieDBMutBuilder::new(&mut *db_write, &mut root).build();
         let value = trie.remove(key).map_err(|e| {
             let msg = format!("Failed to get value for key: {:?}", e);
             error!("{}", msg);
@@ -96,9 +108,7 @@ impl DBStorage for Sha256MemoryStorage {
 
         let value = value.unwrap();
         let value = match value {
-            Value::Inline(v) | Value::NewNode(_,v) => {
-                v.to_vec()
-            }
+            Value::Inline(v) | Value::NewNode(_, v) => v.to_vec(),
             Value::Node(hash) => {
                 use hash_db::HashDBRef;
 
@@ -123,7 +133,7 @@ impl DBStorage for Sha256MemoryStorage {
         let mut db_write = self.db.write().unwrap();
         let mut root = self.root.write().unwrap();
 
-        let mut trie = Sha256TrieDBMutBuilder::new(&mut *db_write, &mut root).build();
+        let mut trie = GenericTrieDBMutBuilder::new(&mut *db_write, &mut root).build();
         trie.commit();
 
         // let new_root = trie.root_hash().to_vec();
@@ -131,8 +141,9 @@ impl DBStorage for Sha256MemoryStorage {
         Ok(())
     }
 
-    async fn root(&self) -> <Self::Hasher as Hasher>::Out {
-        *self.root.read().unwrap()
+    async fn root(&self) -> Vec<u8> {
+        let root = *self.root.read().unwrap();
+        root.as_ref().to_vec()
     }
 
     async fn generate_proof(&self, key: &[u8]) -> NdnResult<Vec<Vec<u8>>> {
@@ -140,12 +151,42 @@ impl DBStorage for Sha256MemoryStorage {
         let root = self.root.read().unwrap();
         // let trie = Sha256TrieDBBuilder::new(&*db_read, &*self.root.read().unwrap()).build();
 
-        let proof = generate_proof::<_, Sha256Layout, _, &[u8]>(&*db_read, &root, &vec![key]).map_err(|e| {
+        let proof = generate_proof::<_, GenericLayout<H>, _, &[u8]>(&*db_read, &root, &vec![key])
+            .map_err(|e| {
             let msg = format!("Failed to generate proof for key: {:?}", e);
             error!("{}", msg);
             NdnError::DbError(msg)
         })?;
-       
+
         Ok(proof)
+    }
+}
+
+pub type Sha256MemoryStorage = GenericMemoryStorage<Sha256Hasher>;
+pub type Sha512MemoryStorage = GenericMemoryStorage<Sha512Hasher>;
+pub type Blake2s256MemoryStorage = GenericMemoryStorage<Blake2s256Hasher>;
+pub type Keccak256MemoryStorage = GenericMemoryStorage<Keccak256Hasher>;
+
+pub struct PathObjectMapInnerStorageFactory {}
+
+impl PathObjectMapInnerStorageFactory {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn create_memory_storage<H: Hasher + Send + Sync + 'static>(
+    ) -> NdnResult<Box<dyn PathObjectMapInnerStorage>> {
+        Ok(Box::new(GenericMemoryStorage::<H>::new()))
+    }
+
+    pub fn create_memory_storage_by_hash_method(
+        hash_method: HashMethod,
+    ) -> NdnResult<Box<dyn PathObjectMapInnerStorage>> {
+        match hash_method {
+            HashMethod::Sha256 => Self::create_memory_storage::<Sha256Hasher>(),
+            HashMethod::Sha512 => Self::create_memory_storage::<Sha512Hasher>(),
+            HashMethod::Blake2s256 => Self::create_memory_storage::<Blake2s256Hasher>(),
+            HashMethod::Keccak256 => Self::create_memory_storage::<Keccak256Hasher>(),
+        }
     }
 }
