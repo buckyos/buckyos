3aebdd7ae31303a5687b58c9bcbe3a0fca4e5562
Weiqiushi <weiqiushi@buckyos.com>
2025-05-05 16:19:30 +0800

    use munkipkg to pack macos installer

---
+++ src/scripts/make_pkg.py
@@ -0,0 +1,65 @@
+# make macos pkg file using munkipkg
+import sys
+from datetime import datetime
+import os
+import shutil
+import json
+import subprocess
+
+import perpare_installer
+
+src_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+rootfs_dir = os.path.join(src_dir, "rootfs")
+pkg_dir = os.path.join(src_dir, "publish", "macos_pkg")
+payload_dir = os.path.join(pkg_dir, "payload")
+dest_dir = os.path.join(payload_dir, "opt", "buckyos")
+
+
+def make_pkg(channel, version, builddate, noBuild):
+    shutil.rmtree(payload_dir, ignore_errors=True)
+
+    # prepare package payload
+    perpare_installer.prepare_installer(dest_dir, channel, "macos", "amd64", version, builddate)
+    
+    print(f'setting pkg version: {version}-{builddate}')
+    # modify build-info.json
+    build_info = json.load(open(os.path.join(pkg_dir, "build-info.json")))
+
+    build_info["version"] = f'{version}-{builddate}'
+
+    json.dump(build_info, open(os.path.join(pkg_dir, "build-info.json"), "w"))
+    print(f"# write build-info.json to {pkg_dir} OK ")
+
+    subprocess.run(["chmod", "+x", os.path.join(pkg_dir, "scripts", "*")], check=True)
+
+    if not noBuild:
+        subprocess.run(["munkipkg", pkg_dir], check=True)
+        print(f"# build pkg to {pkg_dir} OK ")
+        # copy pkg to src_dir
+        pkg_file = os.path.join(pkg_dir, "build", f"macos_pkg-{version}-{builddate}.pkg")
+        if os.path.exists(pkg_file):
+            shutil.copy(pkg_file, os.path.join(src_dir, f"buckyos-{channel}-{version}-{builddate}.pkg"))
+            print(f"# copy pkg to {src_dir} OK ")
+        else:
+            print(f"# pkg file not found: {pkg_file}")
+
+if __name__ == "__main__":
+    print("make sure YOU already run build.py!!!")
+
+    version = "0.4.0"
+    builddate = datetime.now().strftime("%Y%m%d")
+    channel = "nightly"
+
+    for arg in sys.argv:
+        if arg == "--no-build":
+            noBuild = True
+        elif arg.startswith("--builddate="):
+            date = arg.split("=")[1]
+        elif arg.startswith("--version="):
+            version = arg.split("=")[1]
+        elif arg.startswith("--channel="):
+            channel = arg.split("=")[1]
+        else:
+            version = arg
+
+    make_pkg(channel, version, builddate, noBuild)
