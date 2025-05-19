285c118263e517257c837e11ed4aadc1728ac9a7
alexsunxl <alexsunxl@163.com>
2025-05-19 14:51:49 +0800

    Optimize VM clean

--- src/scripts/remote/main.py
+++ src/scripts/remote/main.py
@@ -12,6 +12,7 @@ import stop
 import remote_device
 import py_src.util as util
 import py_src.clog as clog
+import py_src.clean as clean
 
 
 # 配置文件路径
@@ -77,15 +78,6 @@ def network():
 
 
 
-def clean():
-    try:
-        # 调用 multipass 执行清除操作
-        subprocess.run(['multipass', 'delete', '--all', '--purge'], check=True)
-        print("Multipass 实例已成功清除。")
-    except subprocess.CalledProcessError as e:
-        print(f"清除 Multipass 实例时出错: {e}")
-    except FileNotFoundError:
-        print("未找到 multipass 命令，请检查是否已安装。")
 
 
 def init(): 
@@ -176,7 +168,7 @@ def main():
     # parse command
     match sys.argv[1]:
         case "clean":
-            clean()
+            clean.cleanInstances()
             return
         case "init":
             init()
