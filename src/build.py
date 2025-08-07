91730ab1d23c6eb3fa4f8c24fbb8256a52edce2d
waterflier <waterflier@gmail.com>
2025-08-07 15:27:02 +0800

    fix script bugs

--- src/build.py
+++ src/build.py
@@ -1,6 +1,12 @@
+
 import subprocess
 import platform
 import sys
+import os
 
+build_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(build_dir, "scripts"))
+import build
 
-subprocess.run(["python3", "scripts/build.py", *sys.argv[1:]], check=True)
+if __name__ == "__main__":
+    build.build_main()
