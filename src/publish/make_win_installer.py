17435e41adb52ad3e6d86e82681aceb57601d794
weiqiushi <weiqiushi@buckyos.com>
2025-08-21 17:30:52 +0800

    Can set build root dir through env

--- src/publish/make_win_installer.py
+++ src/publish/make_win_installer.py
@@ -6,32 +6,32 @@ import perpare_offical_installer
 from pathlib import Path
 
 src_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-rootfs_dir = os.path.join(src_dir, "rootfs")
 installer_script = os.path.join(src_dir, "publish", "installer.iss")
-result_base_dir = "/opt/buckyosci/publish"
-dest_dir = Path("/") / "opt" / "buckyosci" / "windows-installer"
+result_root_dir = os.environ.get("BUCKYOS_BUILD_DIR", "/opt/buckyosci")
+publish_dir = os.path.join(result_root_dir, "publish_dir")
+tmp_install_dir = os.path.join(result_root_dir, "win-installer")
 
 def make_installer(architecture, version):
     print(f"make installer with architecture: {architecture}, version: {version}")
-    result_dir = os.path.join(result_base_dir, version)
+    result_dir = os.path.join(publish_dir, version)
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
 
-    shutil.rmtree(dest_dir, ignore_errors=True)
-    os.makedirs(dest_dir)
+    shutil.rmtree(tmp_install_dir, ignore_errors=True)
+    os.makedirs(tmp_install_dir)
 
-    shutil.copy(installer_script, dest_dir)
-    print(f"copy installer script to {dest_dir}")
+    shutil.copy(installer_script, tmp_install_dir)
+    print(f"copy installer script to {tmp_install_dir}")
 
     # dest_dir is rootfs, collection items to this NEW rootfs
-    perpare_offical_installer.prepare_rootfs_for_installer(dest_dir / "rootfs",  "windows", architecture, version)
+    perpare_offical_installer.prepare_rootfs_for_installer(tmp_install_dir / "rootfs",  "windows", architecture, version)
 
-    print(f"run build in {dest_dir}")
+    print(f"run build in {tmp_install_dir}")
     iscc_arch = "x64os" if architecture == "amd64" else "arm64os"
-    subprocess.run(f"iscc /DMyAppVersion={version} /DAllowArch={iscc_arch} .\\installer.iss", shell=True, check=True, cwd=dest_dir)
-    print(f"build installer success at {dest_dir}")
-    shutil.copy(f"{dest_dir}/buckyos-{iscc_arch}-{version}.exe", os.path.join(result_dir, f"buckyos-windows-{architecture}-{version}.exe"))
-    print(f"copy installer to {result_base_dir}")
+    subprocess.run(f"iscc /DMyAppVersion={version} /DAllowArch={iscc_arch} .\\installer.iss", shell=True, check=True, cwd=tmp_install_dir)
+    print(f"build installer success at {tmp_install_dir}")
+    shutil.copy(f"{tmp_install_dir}/buckyos-{iscc_arch}-{version}.exe", os.path.join(result_dir, f"buckyos-windows-{architecture}-{version}.exe"))
+    print(f"copy installer to {result_dir}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
