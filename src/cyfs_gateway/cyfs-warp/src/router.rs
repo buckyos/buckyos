e654cee97657240d282dc06c777d4ab88cf49324
alexsunxl <alexsunxl@163.com>
2025-09-08 12:02:14 +0800

    gateway tunnel handle add log

--- src/cyfs_gateway/cyfs-warp/src/router.rs
+++ src/cyfs_gateway/cyfs-warp/src/router.rs
@@ -366,6 +366,7 @@ impl Router {
         if sn_server.is_some() {
             let sn_server = sn_server.unwrap();
             let req_path = req.uri().path();
+            trace!("tunnel_selector handle host:{host} req_path:{req_path}");
             let tunnel_url = sn_server.select_tunnel_for_http_upstream(host,req_path).await;
             if tunnel_url.is_some() {
                 let tunnel_url = tunnel_url.unwrap();
@@ -375,8 +376,7 @@ impl Router {
         } else {
             warn!("No sn server found for selector: {}",selector_id);
         }
-
-        return Err(RouterError::BadGateway("No tunnel selected".to_string()));
+        Err(RouterError::BadGateway("No tunnel selected".to_string()))
     }
 
     async fn handle_upstream(&self, req: Request<Body>, upstream: &UpstreamRouteConfig) -> RouterResult<Response<Body>> {
