a066ef40087fa66ab0740a9a08187cac4398dc9a
weiqiushi <weiqiushi@buckyos.com>
2026-09-08 15:52:04 +0800

    ci: build on push and use shared installer

--- src/publish/win_pkg/scripts/buckyos_preuninstall.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$TaskName = "BuckyOSNodeDaemonKeepAlive"
-
-& schtasks.exe /Delete /TN $TaskName /F *> $null
-
-$RunKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-Remove-ItemProperty -Path $RunKey -Name "BuckyOSDaemon" -ErrorAction SilentlyContinue
